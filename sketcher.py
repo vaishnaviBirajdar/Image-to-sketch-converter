76e46763d5921c9553be55f154991e3b1e9cd2c9
vaishnaviBirajdar <81165132+vaishnaviBirajdar@users.noreply.github.com>
2021-03-31 19:06:18 +0530

    first commit

--- sketcher.py
+++ sketcher.py
@@ -7,7 +7,7 @@ import cv2
 
 #Get the image location and image file name
 img_location = 'D:/self-project( Cartonifier)/'
-filename = 'vaish.jpeg'
+filename = 'puppy.jpeg'
 
 #read in the image
 img = cv2.imread(img_location+filename)
